6b7132eff1ddb23ec1c0a2b4eccb1e1d1645b0b9
Dheeraj <dheerajw2309@gmail.com>
2025-06-16 13:12:49 +0530

    fine

--- train.py
+++ train.py
@@ -1,76 +1,123 @@
 import sys
 import os
-
 import warnings
-
-from model import CANNet
-
-from utils import save_checkpoint
+import time
+import json
+import argparse # Import argparse
 
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
 from torchvision import datasets, transforms
-
 import numpy as np
-import argparse
-import json
-import cv2
-import dataset
-import time
 
-parser = argparse.ArgumentParser(description='PyTorch CANNet')
+# These imports assume the files are in the same directory or accessible
+from model import CANNet
+from utils import save_checkpoint
+import dataset 
 
+# --- Argument Parser ---
+# This section replaces the hardcoded configuration
+parser = argparse.ArgumentParser(description='PyTorch CANNet Fine-Tuning')
+
+# Path Arguments
 parser.add_argument('train_json', metavar='TRAIN',
                     help='path to train json')
 parser.add_argument('val_json', metavar='VAL',
                     help='path to val json')
+parser.add_argument('--pretrained_model', type=str, default=None,
+                    help='path to pre-trained model for fine-tuning')
+parser.add_argument('--save_path', type=str, default='./checkpoints/',
+                    help='path to save checkpoints')
 
+# Hyperparameter Arguments
+parser.add_argument('--lr', type=float, default=1e-5,
+                    help='learning rate for fine-tuning')
+parser.add_argument('--batch_size', type=int, default=4,
+                    help='batch size for training')
+parser.add_argument('--epochs', type=int, default=100,
+                    help='number of epochs to train')
+parser.add_argument('--workers', type=int, default=2,
+                    help='number of data loading workers')
+parser.add_argument('--decay', type=float, default=5e-4,
+                    help='weight decay')
+parser.add_argument('--print_freq', type=int, default=20,
+                    help='print frequency')
+
+# --- Main Function ---
 def main():
-
-    global args,best_prec1
-
+    global args, best_prec1
     best_prec1 = 1e6
 
     args = parser.parse_args()
-    args.lr = 1e-4
-    args.batch_size    = 26
-    args.decay         = 5*1e-4
-    args.start_epoch   = 0
-    args.epochs = 1000
-    args.workers = 4
-    args.seed = int(time.time())
-    args.print_freq = 4
-    with open(args.train_json, 'r') as outfile:
-        train_list = json.load(outfile)
-    with open(args.val_json, 'r') as outfile:
-        val_list = json.load(outfile)
 
-    torch.cuda.manual_seed(args.seed)
+    # Create save directory if it doesn't exist
+    os.makedirs(args.save_path, exist_ok=True)
 
-    model = CANNet()
+    # Check for GPU
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
 
-    model = model.cuda()
+    # --- Data Loading ---
+    with open(args.train_json, 'r') as f:
+        train_list = json.load(f)
+    with open(args.val_json, 'r') as f:
+        val_list = json.load(f)
 
-    criterion = nn.MSELoss(size_average=False).cuda()
+    # --- Model Definition ---
+    model = CANNet().to(device)
 
+    # --- Loss and Optimizer ---
+    criterion = nn.MSELoss(size_average=False).to(device)
     optimizer = torch.optim.Adam(model.parameters(), args.lr,
-                                    weight_decay=args.decay)
+                                 weight_decay=args.decay)
 
-    for epoch in range(args.start_epoch, args.epochs):
-        train(train_list, model, criterion, optimizer, epoch)
-        prec1 = validate(val_list, model, criterion)
+    # --- Load Pre-trained Weights for Fine-Tuning ---
+    if args.pretrained_model:
+        if os.path.isfile(args.pretrained_model):
+            print(f"=> loading checkpoint '{args.pretrained_model}'")
+            checkpoint = torch.load(args.pretrained_model, map_location=device)
+            # You might need to adjust this if the checkpoint structure is different
+            model.load_state_dict(checkpoint['state_dict'])
+            print(f"=> loaded checkpoint '{args.pretrained_model}' successfully")
+        else:
+            print(f"=> ERROR: No checkpoint found at '{args.pretrained_model}'")
+            # Exit if the specified pre-trained model doesn't exist
+            return
+    else:
+        print("=> No pre-trained model provided. Training from scratch.")
+
+    # --- Training Loop ---
+    for epoch in range(args.epochs):
+        # Here you could implement a learning rate scheduler if needed
+        # adjust_learning_rate(optimizer, epoch) 
+        
+        train(train_list, model, criterion, optimizer, epoch, device)
+        prec1 = validate(val_list, model, criterion, device)
 
         is_best = prec1 < best_prec1
         best_prec1 = min(prec1, best_prec1)
-        print(' * best MAE {mae:.3f} '
-              .format(mae=best_prec1))
+        print(f' * Best MAE so far {best_prec1:.3f}')
+        
+        # Save checkpoints to the specified path
         save_checkpoint({
+            'epoch': epoch + 1,
             'state_dict': model.state_dict(),
-        }, is_best)
+            'best_prec1': best_prec1,
+        }, is_best, filename=os.path.join(args.save_path, 'checkpoint.pth.tar'))
 
-def train(train_list, model, criterion, optimizer, epoch):
 
+# --- Helper classes and functions ---
+class AverageMeter(object):
+    def __init__(self): self.reset()
+    def reset(self): self.val, self.avg, self.sum, self.count = 0, 0, 0, 0
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+def train(train_list, model, criterion, optimizer, epoch, device):
     losses = AverageMeter()
     batch_time = AverageMeter()
     data_time = AverageMeter()
@@ -79,102 +126,72 @@ def train(train_list, model, criterion, optimizer, epoch):
         dataset.listDataset(train_list,
                        shuffle=True,
                        transform=transforms.Compose([
-                       transforms.ToTensor(),transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225]),
-                   ]),
-                       train=True,
-                       seen=model.seen,
+                           transforms.ToTensor(),
+                           transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                std=[0.229, 0.224, 0.225]),
+                       ]), 
+                       train=True, 
                        batch_size=args.batch_size,
                        num_workers=args.workers),
         batch_size=args.batch_size)
-    print('epoch %d, processed %d samples, lr %.10f' % (epoch, epoch * len(train_loader.dataset), args.lr))
-
+    
+    print(f'Epoch: {epoch}, lr: {optimizer.param_groups[0]["lr"]:.5f}')
     model.train()
     end = time.time()
 
-    for i,(img, target)in enumerate(train_loader):
+    for i, (img, target) in enumerate(train_loader):
         data_time.update(time.time() - end)
 
-        img = img.cuda()
-        img = Variable(img)
-        output = model(img)[:,0,:,:]
-
-        target = target.type(torch.FloatTensor).cuda()
-        target = Variable(target)
-
-        loss = criterion(output, target)
-
+        img = img.to(device)
+        target = target.type(torch.FloatTensor).to(device)
+        
+        output = model(img)
+        
+        loss = criterion(output.squeeze(), target)
+        
         losses.update(loss.item(), img.size(0))
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-
+        
         batch_time.update(time.time() - end)
         end = time.time()
-
+        
         if i % args.print_freq == 0:
-            print('Epoch: [{0}][{1}/{2}]\t'
-                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                  .format(
-                   epoch, i, len(train_loader), batch_time=batch_time,
-                   data_time=data_time, loss=losses))
+            print(f'  [{i}/{len(train_loader)}]\t'
+                  f'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  f'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  f'Loss {losses.val:.4f} ({losses.avg:.4f})')
 
-def validate(val_list, model, criterion):
-    print ('begin val')
+def validate(val_list, model, criterion, device):
+    print('begin val')
     val_loader = torch.utils.data.DataLoader(
     dataset.listDataset(val_list,
                    shuffle=False,
                    transform=transforms.Compose([
-                       transforms.ToTensor(),transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225]),
-                   ]),  train=False),
-    batch_size=1)
+                       transforms.ToTensor(),
+                       transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                            std=[0.229, 0.224, 0.225]),
+                   ]), train=False),
+    batch_size=1) # Validate one image at a time
 
     model.eval()
-
     mae = 0
-
-    for i,(img, target) in enumerate(val_loader):
-        h,w = img.shape[2:4]
-        h_d = h/2
-        w_d = w/2
-        img_1 = Variable(img[:,:,:h_d,:w_d].cuda())
-        img_2 = Variable(img[:,:,:h_d,w_d:].cuda())
-        img_3 = Variable(img[:,:,h_d:,:w_d].cuda())
-        img_4 = Variable(img[:,:,h_d:,w_d:].cuda())
-        density_1 = model(img_1).data.cpu().numpy()
-        density_2 = model(img_2).data.cpu().numpy()
-        density_3 = model(img_3).data.cpu().numpy()
-        density_4 = model(img_4).data.cpu().numpy()
-
-        pred_sum = density_1.sum()+density_2.sum()+density_3.sum()+density_4.sum()
-
-        mae += abs(pred_sum-target.sum())
-
-    mae = mae/len(val_loader)
-    print(' * MAE {mae:.3f} '
-              .format(mae=mae))
-
+    
+    with torch.no_grad():
+        for i,(img, target) in enumerate(val_loader):
+            img = img.to(device)
+            output = model(img)
+            
+            # Sum of the density map gives the count
+            pred_count = output.sum().item()
+            gt_count = target.sum().item()
+            
+            mae += abs(pred_count - gt_count)
+            
+    mae = mae / len(val_loader)    
+    print(f' * MAE {mae:.3f}')
     return mae
-
-class AverageMeter(object):
-    """Computes and stores the average and current value"""
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
 
 if __name__ == '__main__':
     main()
